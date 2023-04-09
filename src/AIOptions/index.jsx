d7b84223d02f3b3953d5352caf99cc3c023661b7
Niwaiwa <po2846@gmail.com>
2023-04-09 23:47:05 +0800

    Update index.jsx
    add openai example to index.jsx

--- src/AIOptions/index.jsx
+++ src/AIOptions/index.jsx
@@ -147,4 +147,485 @@ export const arrayItems = [
             presence_penalty: 0.0
         }
     },
+    {
+        name: 'Movie to Emoji',
+        id: 'movieToEmoji',
+        description: 'Convert movie titles into emoji.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.8,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\n"],
+        }
+    },
+    {
+        name: 'Calculate Time Complexity',
+        id: 'calculateTimeComplexity',
+        description: 'Find the time complexity of a function.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\n"],
+        }
+    },
+    {
+        name: 'Translate programming languages',
+        id: 'translateProgrammingLanguages',
+        description: 'Translate from one programming language to another.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["###"],
+        }
+    },
+    {
+        name: 'Advanced tweet classifier',
+        id: 'advancedTweetClassifier',
+        description: 'Advanced sentiment detection for a piece of text.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Explain code',
+        id: 'explainCode',
+        description: 'Explain a complicated piece of code.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\"\"\""],
+        }
+    },
+    {
+        name: 'Keywords',
+        id: 'keywords',
+        description: 'Extract keywords from a block of text.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.8,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Factual answering',
+        id: 'factualAnswering',
+        description: 'Direct the model to provide factual answers and address knowledge gaps.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.8,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Ad from product description',
+        id: 'adFromProductDescription',
+        description: 'Turn a product description into ad copy.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 100,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Product name generator',
+        id: 'productNameGenerator',
+        description: 'Create product names from examples words.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.8,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'TL;DR summarization',
+        id: 'tldrSummarization',
+        description: "Summarize text by adding a 'tl;dr:' to the end of a text passage.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.7,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 1,
+        }
+    },
+    {
+        name: 'Python bug fixer',
+        id: 'pythonBugFixer',
+        description: "Find and fix bugs in source code.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 182,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["###"],
+        }
+    },
+    {
+        name: 'Spreadsheet creator',
+        id: 'spreadsheetCreator',
+        description: "Create spreadsheets of various kinds of data.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'JavaScript helper chatbot',
+        id: 'javascriptHelperChatbot',
+        description: "Message-style bot that answers JavaScript questions.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.0,
+            stop: ["You:"],
+        }
+    },
+    {
+        name: 'ML/AI language model tutor.',
+        id: 'mlaiLanguageModelTutor',
+        description: "Bot that answers questions about language models",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.3,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.0,
+            stop: ["You:"],
+        }
+    },
+    {
+        name: 'Science fiction book list maker',
+        id: 'scienceFictionBookListMaker',
+        description: "Create a list of items for a given topic.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 200,
+            top_p: 1.0,
+            frequency_penalty: 0.52,
+            presence_penalty: 0.5,
+            stop: ["11."],
+        }
+    },
+    {
+        name: 'Tweet classifier',
+        id: 'tweetClassifier',
+        description: "Basic sentiment detection for a piece of text.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Airport code extractor',
+        id: 'airportCodeExtractor',
+        description: "Extract airport codes from text.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\n"],
+        }
+    },
+    {
+        name: 'SQL request',
+        id: 'sqlRequest',
+        description: "Create simple SQL queries.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.3,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Extract contact information',
+        id: 'extractContactInformation',
+        description: "Extract contact information from a block of text.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'JavaScript to Python',
+        id: 'javascriptToPython',
+        description: "Convert simple JavaScript expressions into Python.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Mood to color',
+        id: 'moodToColor',
+        description: "Turn a text description into a color.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: [";"],
+        }
+    },
+    {
+        name: 'Write a Python docstring',
+        id: 'writeAPythonDocstring',
+        description: "Write a docstring for a Python function.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["#", "\"\"\""],
+        }
+    },
+    {
+        name: 'Analogy maker',
+        id: 'analogyMaker',
+        description: "Create analogies.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'JavaScript one line function',
+        id: 'javascriptOneLineFunction',
+        description: "Turn a JavaScript function into a one liner.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: [";"],
+        }
+    },
+    {
+        name: 'Micro horror story creator',
+        id: 'microHorrorStoryCreator',
+        description: "Creates two to three sentence short horror stories from a topic input.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.8,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Third-person converter',
+        id: 'thirdPersonConverter',
+        description: "Converts first-person POV to the third-person.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Notes to summary',
+        id: 'notesToSummary',
+        description: "Turn meeting notes into a summary.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'VR fitness idea generator',
+        id: 'vrFitnessIdeaGenerator',
+        description: "Create ideas for fitness and virtual reality games.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.6,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 1,
+            presence_penalty: 1,
+        }
+    },
+    {
+        name: 'Essay outline',
+        id: 'essayOutline',
+        description: "Generate an outline for a research topic.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.3,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Recipe creator (eat at your own risk)',
+        id: 'recipeCreator',
+        description: "Create a recipe from a list of ingredients.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.3,
+            max_tokens: 120,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Chat',
+        id: 'chat',
+        description: "Open ended conversation with an AI assistant.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.9,
+            max_tokens: 150,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.6,
+            stop: [" Human:", " AI:"],
+        }
+    },
+    {
+        name: 'Marv the sarcastic chat bot',
+        id: 'marvTheSarcasticChatBot',
+        description: "Marv is a factual chatbot that is also sarcastic.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 60,
+            top_p: 0.3,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Turn by turn directions',
+        id: 'turnByTurnDirections',
+        description: "Convert natural language to turn-by-turn directions.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.3,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Restaurant review creator',
+        id: 'restaurantReviewCreator',
+        description: "Turn a few words into a restaurant review.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Create study notes',
+        id: 'createStudyNotes',
+        description: "Provide a topic and get study notes.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.3,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
+    {
+        name: 'Interview questions',
+        id: 'interviewQuestions',
+        description: "Create interview questions.",
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.5,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+    },
 ]
